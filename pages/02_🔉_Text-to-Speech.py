ba96b24e716e4b1d88d4b918a46505f87eabe64b
valiticat <106557000+valiticat@users.noreply.github.com>
2022-09-05 10:44:37 +0300

    upd

--- pages/02_🔉_Text-to-Speech.py
+++ pages/02_🔉_Text-to-Speech.py
@@ -52,7 +52,7 @@ if input_text != '':
     )
     speech_.write_to_fp(speech)
     
-    playCol, emptyCol = st.columns([1.0, 1.0])
+    playCol, emptyCol = st.columns([0.9, 1.0])
     
     with playCol:
         st.write("Check ")
