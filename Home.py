c3fef3db4fab02fd3ebedebc04e406f62c1efef8
valiticat <106557000+valiticat@users.noreply.github.com>
2023-07-08 09:31:03 +0300

    rrr

--- Home.py
+++ Home.py
@@ -6,10 +6,4 @@ st.set_page_config(
    page_title="Demo",
    page_icon="📈",
    layout="centered",
-   initial_sidebar_state="expanded",
 )
-
-st.header("Hello, world!")
-st.subheader("My name is Pavlo, and this is my Data Science Portfolio")
-
-st.write("Please check the 'Text-to-Speech' page")
